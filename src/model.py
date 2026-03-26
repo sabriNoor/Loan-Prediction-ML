3cc2cf70fbac77e59f02a09f487d15a55ce0ab0d
sabriNoor <nooraosama63@gmail.com>
2026-03-26 15:51:07 +0200

    feat: implement LoanModel class using Logistic Regression

---
+++ src/model.py
@@ -0,0 +1,11 @@
+from sklearn.linear_model import LogisticRegression
+
+class LoanModel:
+    def __init__(self):
+        self.model = LogisticRegression(max_iter=1000)
+
+    def train(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
